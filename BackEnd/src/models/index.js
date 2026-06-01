32acba06c7b6e087d3a7e789ad7fa8c120e62292
BrunoGojo <brunardoxs84@gmail.com>
2026-06-01 19:42:22 -0300

    Update package.json

--- BackEnd/src/models/index.js
+++
@@ -1,4 +0,0 @@
-export { createUser, UserRoles, UserSchema } from './User.js';
-export { createLaudo, createAIResult, LaudoStatus } from './Laudo.js';
-export { createHistorico, HistoricoAcao } from './Historico.js';
-export { createMetrics } from './Metrics.js';
